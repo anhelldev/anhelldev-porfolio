ccd63a3f087bb3445b60548eab6bea744a480851
Angel Larreal <angellarreal95@gmail.com>
2026-02-05 02:56:28 -0400

    feat: Configure Next.js images with `unoptimized: true` for static export.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Recomendado para evitar errores con next/image en static export
+  },
 }
 
 export default nextConfig
